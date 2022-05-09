9eeb923c8007267af034b471553eac72d432f6ec
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-05-08 21:25:53 -0500

    first commit

---
+++ src/test/java/com/formacionbdi/springboot/app/config/springbootservicioconfigserver/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.config.springbootservicioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
